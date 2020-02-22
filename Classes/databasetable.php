8ca5d6dca7545a57866704fba87f868cad08f55f
aadityabhusal <bhusal.001aditya@gmil.com>
2020-02-22 22:08:19 +0545

    CSV File data insert into database table

--- Classes/databasetable.php
+++ Classes/databasetable.php
@@ -1,10 +1,31 @@
 <?php
 
 class Databasetable{
-    private $table; private $pdo;private $field; private $value;
+    private $table; 
+    private $pdo;
+    private $field; 
+    private $value;
+
     function __construct($table){
         $this->pdo = new PDO('mysql:dbname=csy2028groupassignment;host=localhost','root','');
         $this->table = $table;
+    }
+
+    function insert($record){
+        $keys = array_keys($record);
+        $keysComma = implode(', ', $keys);
+        $keysColon = implode(', :', $keys);
+        $stmt = $this->pdo->prepare("INSERT INTO $this->table($keysComma) VALUES(:$keysColon)");
+        if($stmt->execute($record))
+            return true;
+        else
+            return false;
+    }
+
+    function findAll(){
+        $stmt = $this->pdo->prepare("SELECT * FROM $this->table");
+        $stmt->execute();
+        return $stmt;
     }
 }
 ?>
--- adminDash.php
+++ adminDash.php
@@ -1,10 +1,33 @@
 <?php
     include 'header.php';
+    include 'classes/databasetable.php';
+    $students = new Databasetable('admission');
+
+    if(isset($_POST['upload'])){
+        $csvFileName = explode(".", $_FILES['UCASDetail']['name']);
+        if(end($csvFileName) == "csv"){
+            $csvHandle = fopen($_FILES['UCASDetail']['tmp_name'], "r");
+            $mydata = fgetcsv($csvHandle);
+            while($data = fgetcsv($csvHandle) ){
+                $arr = [];
+                $count = 0;
+                foreach($data as $key => $value){
+                    $arr[$mydata[$count]] = $value;
+                    $count++;
+                }
+                $students->insert($arr);
+            }
+        }
+    }
+
 ?>
 <html>
     <main>
-        <input type="submit" name="UCASDetail" value="Upload Student Details" class="Button">
-        <br>
+        <form action="" method="post" enctype='multipart/form-data'>
+            <input type="file" name="UCASDetail" class="Button">
+            <br>
+            <input type="submit" name="upload" value="Upload" class="Button">
+        </form>
         <input type="submit" name="Filter" value="Filter" class="Button">
     </main>
     <?php
