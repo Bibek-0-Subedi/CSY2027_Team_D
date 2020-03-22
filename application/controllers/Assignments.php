d33e3b774f0ad27ea4b8e851ced04fce066ce1fd
sudeshna02 <46808019+sudeshna02@users.noreply.github.com>
2020-03-22 13:21:57 +0545

    assignment with access

--- application/controllers/Assignments.php
+++ application/controllers/Assignments.php
@@ -4,11 +4,8 @@ class Assignments extends CI_Controller {
 
 	 public function loadViews($page, $title, $data = []) {
         $this->load->view('layouts/header', ['title' => $title]);
-<<<<<<< HEAD
+
         $this->load->view('layouts/siteNav');
-=======
-        $this->load->view('layouts/adminNav');
->>>>>>> b6e7c7a9c729bb2ca4f0ee0c330019c6283d66a1
         $this->load->view('assignment/'.$page, $data);
         $this->load->view('layouts/footer');
     }
